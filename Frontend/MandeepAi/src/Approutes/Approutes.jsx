b99ef8be8b3a53b23fd15cb9f108a7baf5b5e799
kuldeepsengar01 <kuldeepsengar8218@gmail.com>
2026-08-15 10:40:16 +0530

    Add ProtectedRoute for Home and root paths

--- Frontend/MandeepAi/src/Approutes/Approutes.jsx
+++ Frontend/MandeepAi/src/Approutes/Approutes.jsx
@@ -1,39 +1,69 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 
 import Register from '../component/Register';
 import Otp from '../component/Otp';
 import Login from '../component/Login';
 import Home from '../component/Home';
 
+const ProtectedRoute = ({ children }) => {
+
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
 const AppRoutes = () => {
+
   return (
     <Router>
       <Routes>
 
+        {/* Home */}
         <Route
           path="/"
-          element={<Home />}
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
         />
 
+        {/* Register */}
         <Route
           path="/register"
           element={<Register />}
         />
 
+        {/* OTP */}
         <Route
           path="/verify-otp"
           element={<Otp />}
         />
 
+        {/* Login */}
         <Route
           path="/login"
           element={<Login />}
         />
 
+        {/* Home */}
         <Route
           path="/home"
-          element={<Home />}
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
         />
 
         {/* Unknown URL */}
